5532d70c5d4c126fe3769929cab91353f5c17e6d
dericzin <81449645+dericzin@users.noreply.github.com>
2024-11-05 09:11:07 -0300

    Add files via upload
    Site v2

--- script.js
+++ script.js
@@ -26,21 +26,102 @@ const quizzes = {
         ]
     },
     osso2: {
-        name: "Osso 2", // Nome do osso
-        image: "osso2.jpg", // Substitua com a imagem correta
+        name: "Crânio Equino - Lateral", // Nome do osso
+        image: "cranio_equino_lateral.png", // Substitua com a imagem correta
         questions: [
-            { question: "1. Nome do acidente ósseo 4?", answer: "resposta4" },
-            { question: "2. Nome do acidente ósseo 5?", answer: "resposta5" },
-            { question: "3. Nome do acidente ósseo 6?", answer: "resposta6" }
+            { question: "1. Nome do acidente ósseo 15?", answer: "Corpo do Incisivo" },
+            { question: "2. Nome do acidente ósseo 16?", answer: "Processo Nasal do Incisivo" },
+            { question: "3. Nome do acidente ósseo 17?", answer: "Dentes Incisivos" },
+            { question: "4. Nome do acidente ósseo 21?", answer: "Incisura Nasoincisiva" },
+            { question: "5. Nome do acidente ósseo 22?", answer: "Crista Facial" },
+            { question: "6. Nome do acidente ósseo 24?", answer: "Diastema" },
+            { question: "7. Nome do acidente ósseo 25?", answer: "Dentes Caninos" },
+            { question: "8. Nome do acidente ósseo 26?", answer: "Dentes Pré-Molares" },
+            { question: "9. Nome do acidente ósseo 27?", answer: "Dentes Molares" },
+            { question: "10. Nome do acidente ósseo 28?", answer: "Tuberosidade Maxilar" },
+            { question: "11. Nome do acidente ósseo 29?", answer: "Forame Infraorbital" },
+            { question: "12. Nome do acidente ósseo 31?", answer: "Processo Temporal do Zigomático" },
+            { question: "13. Nome do acidente ósseo 32?", answer: "Processo Zigomático do Frontal" },
+            { question: "14. Nome do acidente ósseo 33?", answer: "Processo Zigomático do Temporal" },
+            { question: "15. Nome do acidente ósseo 45?", answer: "Meato Acústico Externo" },
+            { question: "16. Nome do acidente ósseo 53?", answer: "Crista Nucal" },
+            { question: "17. Nome do acidente ósseo 54?", answer: "Côndilo do Occipital" },
+            { question: "18. Nome do acidente ósseo 55?", answer: "Processo Paracondilar do Occipital" }
+
         ]
     },
     osso3: {
-        name: "Osso 3", // Nome do osso
-        image: "osso3.jpg", // Substitua com a imagem correta
+        name: "Crânio Bovino - Lateral", // Nome do osso
+        image: "cranio_bovino_lateral.png", // Substitua com a imagem correta
         questions: [
-            { question: "1. Nome do acidente ósseo 7?", answer: "resposta7" },
-            { question: "2. Nome do acidente ósseo 8?", answer: "resposta8" },
-            { question: "3. Nome do acidente ósseo 9?", answer: "resposta9" }
+            { question: "1. Nome do osso número 1?", answer: "Osso Incisivo" },
+            { question: "2. Nome do osso número 2?", answer: "Osso Nasal" },
+            { question: "3. Nome do osso número 3?", answer: "Osso Maxilar" },
+            { question: "4. Nome do osso número 4?", answer: "Osso Lacrimal" },
+            { question: "5. Nome do osso número 5?", answer: "Osso Zigomático" },
+            { question: "6. Nome do osso número 6?", answer: "Osso Frontal" },
+            { question: "7. Nome do osso número 7?", answer: "Osso Parietal" },
+            { question: "8. Nome do osso número 8?", answer: "Osso Interparietal" },
+            { question: "9. Nome do osso número 9?", answer: "Osso Temporal" },
+            { question: "10. Nome do osso número 10?", answer: "Osso Occipital" },
+            { question: "11. Nome do osso número 11?", answer: "Mandíbula" },
+            { question: "12. Nome do osso número 12?", answer: "Osso Palatino" },
+        ]
+    },
+    osso4: {
+        name: "Crânio Equino - Dorsal", // Nome do osso
+        image: "cranio_equino_dorsal.png", // Substitua com a imagem correta
+        questions: [
+            { question: "1. Nome do acidente ósseo 15?", answer: "Corpo do Incisivo" },
+            { question: "2. Nome do acidente ósseo 16?", answer: "Processo Nasal do Incisivo" },
+            { question: "3. Nome do acidente ósseo 22?", answer: "Crista Facial" },
+            { question: "4. Nome do acidente ósseo 29?", answer: "Forame Infraorbital" },
+            { question: "5. Nome do acidente ósseo 30?", answer: "Forame Supraorbital" },
+            { question: "6. Nome do acidente ósseo 31?", answer: "Processo Temporal do Zigomático" },
+            { question: "7. Nome do acidente ósseo 32?", answer: "Processo Zigomático do Frontal" },
+            { question: "8. Nome do acidente ósseo 33?", answer: "Processo Zigomático do Temporal" },
+            { question: "9. Nome do acidente ósseo 43?", answer: "Linha Temporal" },
+            { question: "10. Nome do acidente ósseo 44?", answer: "Crista Sagital Externa" },
+            { question: "11. Nome do acidente ósseo 46?", answer: "Fossa Temporal" },
+            { question: "12. Nome do acidente ósseo 53?", answer: "Crista Nucal" }
+        ]
+    },
+    osso5: {
+        name: "Crânio Equino - Ventral", // Nome do osso
+        image: "cranio_equino_ventral.png", // Substitua com a imagem correta
+        questions: [
+            { question: "1. Nome do acidente ósseo 15?", answer: "Corpo do Incisivo" },
+            { question: "2. Nome do acidente ósseo 17?", answer: "Dentes Incisivos" },
+            { question: "3. Nome do acidente ósseo 18?", answer: "Processo Palatino do Incisivo" },
+            { question: "4. Nome do acidente ósseo 19?", answer: "Fissura Palatina" },
+            { question: "5. Nome do acidente ósseo 20?", answer: "Canal Interincisivo" },
+            { question: "6. Nome do acidente ósseo 22?", answer: "Crista Facial" },
+            { question: "7. Nome do acidente ósseo 23?", answer: "Processo Palatino da Maxila" },
+            { question: "8. Nome do acidente ósseo 24?", answer: "Diastema" },
+            { question: "9. Nome do acidente ósseo 25?", answer: "Dentes Caninos" },
+            { question: "10. Nome do acidente ósseo 26?", answer: "Dentes Pré-Molares" },
+            { question: "11. Nome do acidente ósseo 27?", answer: "Dentes Molares" },
+            { question: "12. Nome do acidente ósseo 28?", answer: "Tuberosidade Maxilar" },
+            { question: "13. Nome do acidente ósseo 34?", answer: "Lâmina Perpendicular do Palatino" },
+            { question: "14. Nome do acidente ósseo 35?", answer: "Lâmina Horizontal do Palatino" },
+            { question: "15. Nome do acidente ósseo 36?", answer: "Forame Palatino Maior" },
+            { question: "16. Nome do acidente ósseo 37?", answer: "Asa do Vômer" },
+            { question: "17. Nome do acidente ósseo 38?", answer: "Haste do Vômer" },
+            { question: "18. Nome do acidente ósseo 39?", answer: "Coanas" },
+            { question: "19. Nome do acidente ósseo 40?", answer: "Hâmulo do Pterigoide" },
+            { question: "20. Nome do acidente ósseo 41?", answer: "Basoesfenoide" },
+            { question: "21. Nome do acidente ósseo 42?", answer: "Asa do Esfenoide" },
+            { question: "22. Nome do acidente ósseo 47?", answer: "Bula Timpânica" },
+            { question: "23. Nome do acidente ósseo 48?", answer: "Processos Musculares do Temporal" },
+            { question: "24. Nome do acidente ósseo 49?", answer: "Forame Lácero" },
+            { question: "25. Nome do acidente ósseo 50?", answer: "Tubérculo Articular do Temporal" },
+            { question: "26. Nome do acidente ósseo 51?", answer: "Fossa Mandibular do Temporal" },
+            { question: "27. Nome do acidente ósseo 52?", answer: "Processo Retroarticular do Temporal" },
+            { question: "28. Nome do acidente ósseo 54?", answer: "Côndilo do Occipital" },
+            { question: "29. Nome do acidente ósseo 55?", answer: "Processo Paracondilar do Occipital" },
+            { question: "30. Nome do acidente ósseo 56?", answer: "Forame Magno" },
+            { question: "31. Nome do acidente ósseo 57?", answer: "Parte Basilar do Occipital" },
+            { question: "32. Nome do acidente ósseo 58?", answer: "Canal do Nervo Hipoglosso" }
         ]
     }
 };
